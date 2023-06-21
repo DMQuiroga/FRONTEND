c2b11f4cde5be0f35d8bcfd44bd55db3f0a3ffc7
FranValderrey <fran.valderrey@gmail.com>
2023-06-21 15:53:37 +0200

    Creada carpeta componentes y primer componente SearchBar (aún sin funcionalidad)

--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -2,6 +2,7 @@ import { Link, Outlet } from 'react-router-dom';
 import { useUser } from '../context/UserContext';
 import './Index.css';
 import { useAuthentication } from '../hooks/authApi';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 function Index() {
   const [user] = useUser();
@@ -14,6 +15,7 @@ function Index() {
           <Link to="/">
             <h1>HB NEWS</h1>
           </Link>
+          <SearchBar />
           {user ? (
             <p>
               <span className="userName">{user.name}</span>
