a0686fb3b143dc436fd564e820ed908d74b7eb30
Eduardo <eduardocastrobianchi@gmail.com>
2023-06-23 14:20:17 +0200

    componentes-sumados-david+TodayNews

--- src/hooks/newsApi.js
+++ src/hooks/newsApi.js
@@ -1,15 +1,29 @@
-//import { useUser } from '../context/UserContext';
-import useAuthHttpCall from './useAuthHttpCall';
+/* import { useState, useEffect } from 'react';
 
-const BACKEND_URL = 'http://localhost:8080';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export function useNews() {
-  const { get } = useAuthHttpCall();
+function useNews() {
+  const [news, setNews] = useState([]);
 
-  const loadTodayNews = (callback) => {
-    console.log('API CALL');
-    get(BACKEND_URL + '/today-news').then((response) => callback(response));
-  };
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(BACKEND_URL + '/today-news');
+        const data = await response.json();
+        if (data.status === 'ok') {
+          setNews(data.data);
+        } else {
+          console.error('Error al obtener las noticias:', data.message);
+        }
+      } catch (error) {
+        console.error('Error al realizar la solicitud:', error);
+      }
+    };
 
-  return { loadTodayNews };
+    fetchNews();
+  }, []);
+
+  return news;
 }
+
+export default useNews; */
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,70 +1,12 @@
-//import { Navigate } from 'react-router-dom';
-//import { useUser } from '../../context/UserContext';
-import { useEffect, useState } from 'react';
-//import { useNews } from '../../hooks/newsApi';
+import TodayNews from '../../components/TodayNews/TodayNews';
 import './Home.css';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
 function Home() {
-  //const { loadTodayNews } = useNews();
-
-  const [news, setNews] = useState([]);
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        const response = await fetch(BACKEND_URL + '/today-news');
-        const data = await response.json();
-        if (data.status === 'ok') {
-          setNews(data.data);
-        } else {
-          console.error('Error al obtener las noticias:', data.message);
-        }
-      } catch (error) {
-        console.error('Error al realizar la solicitud:', error);
-      }
-    };
-
-    fetchNews();
-  }, []);
-
   return (
     <>
-      {news.length === 0 ? (
-        <p>No se encontraron noticias.</p>
-      ) : (
-        <ul>
-          {news.map((n, index) => (
-            <li className="newscard" key={index}>
-              <h2>{n.title}</h2>
-              <p>{n.text}</p>
-              <img src={n.imagenUrl} alt={n.title} />
-              <div className="score">{n.score} </div>
-              <p>{n.publishDate} </p>
-            </li>
-          ))}
-        </ul>
-      )}
+      <TodayNews />
     </>
   );
 }
-
-/* try {
-    loadTodayNews(setNews);
-  } catch (error) {
-    alert(error);
-    setNews();
-  }
-
-  return (
-    <section>
-      {news?.map((n) => (
-        <div key={n.id}>n.title</div>
-      ))}
-      {JSON.stringify(news)}
-    </section>
-  );
-} */
 
 export default Home;
