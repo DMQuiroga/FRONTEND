12c6a20262dc6ee435d9bcc64277c2fd13e0936d
dmarquiroga@gmail.com <dmarquiroga@gmail.com>
2023-06-23 02:26:47 +0200

    Mejorando style css de web

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -98,10 +98,20 @@ function Signup() {
         Acepto los términos y condiciones
       </label>
       <button>Registro</button>
+
       <p>
         ¿Ya estás registrado?
         <Link to="/login">Inicia sesión</Link>
       </p>
+      <div className="pyramid-loader">
+        <div className="wrapper">
+          <span className="side side1"></span>
+          <span className="side side2"></span>
+          <span className="side side3"></span>
+          <span className="side side4"></span>
+          <span className="shadow"></span>
+        </div>
+      </div>
     </form>
   );
 }
