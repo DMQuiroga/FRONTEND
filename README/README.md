4940783a793ed3b81ae65f05d4470d25b7ac46fd
dmarquiroga@gmail.com <dmarquiroga@gmail.com>
2023-06-30 15:53:06 +0200

    Scorer usando contexto de usuario para si engo user logueado muestro botones de like y dislike, si no tengo user logueado no muestro botones

--- README/README.md
+++ README/README.md
@@ -7,17 +7,17 @@ TO DO NO PRIORITARIO:
 
 # PRIORIDADES!!!!!!!:
 
-1 - Visualizar el usuario y la fotoavatar de usuario en la noticia que fue creada por el.
-2 - Meter dentro del button el logout y hacer que el boton sea el inicio de sesion.
+1 - Visualizar el usuario y la fotoavatar de usuario en la noticia que fue creada por el.----edu
+2 - Meter dentro del button el logout y hacer que el boton sea el inicio de sesion.-----edu
 
 **USUARIOS REGISTRADOS**
-3- Actualizar usuario
+3- Actualizar usuario----edu
 ○○ Nombre
 ○○ Email
 ○○ Biografía
 ○○ Foto
 
-4- Crear noticia
+4- Crear noticia----guille
 
 ● Publicar una nueva noticia:
 ○○ Título
@@ -26,9 +26,9 @@ TO DO NO PRIORITARIO:
 ○○ Texto de la noticia
 ○○ Tema
 
-5- Editar una noticia publicada por el mismo usuario
-6- Borrar una noticia publicada por el usuario
-7- Votar positivamente o negativamente otras noticias------50% Porque no se guardan los votos!
+5- Editar una noticia publicada por el mismo usuario----FRAN
+6- Borrar una noticia publicada por el usuario-----FRAN
+7- Votar positivamente o negativamente otras noticias------50% Porque no se guardan los votos!---DAVID
 
 8- OPCIONAL = ACTUALIZAR USUARIO
-○ Gestión del perfil de usuario (Nombre, Email, Biografía, Foto, …)
+○ Gestión del perfil de usuario (Nombre, Email, Biografía, Foto, …)----edu
--- src/components/Scorer.jsx
+++ src/components/Scorer.jsx
@@ -1,13 +1,45 @@
 import { useState } from 'react';
+import { useUser } from '../context/UserContext';
+
+/* 
+1- FRONTEND: 
+            Coger del contexto el usuario y si no tengo usuario es decir no estoy logueado entonces 
+            los botones de votar negativamente y positivamente no los muestro. Solo muestro el 
+            numero de votaciones.
+2- FRONTEND: Guardar el voto
+      Usar nuestro post de hooks/useAuthHttpCall --- Para
+      NewsApi.jsx => crear una nueva funcion que sea votar
+      No crear dos, es la misma y pasarle por parametro que sea un +1 o un -1
+      Volvemos al componente y hacer que se haga esa llamada al pulsar el botón.
+3- BACKEND: Guardar de alguna forma que el usuario a votado a la noticia 55. Hacer más adelante.
+
+*/
 
 function Scorer({ initial = 0 }) {
   const [valor, setValor] = useState(initial);
+  const [user] = useUser();
+
+  const handleIncrement = () => {
+    if (user) {
+      setValor(valor + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (user && valor > 0) {
+      setValor(valor - 1);
+    }
+  };
 
   return (
     <div className="scorer">
-      <button onClick={() => valor > 0 && setValor(valor - 1)}>-</button>
-      <span>{valor}</span>
-      <button onClick={() => setValor(valor + 1)}>+</button>
+      {user && (
+        <>
+          <button onClick={handleDecrement}>☠️</button>
+          <span>{valor}</span>
+          <button onClick={handleIncrement}>❤️</button>
+        </>
+      )}
     </div>
   );
 }
