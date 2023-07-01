c7ad350418407d539730a29c0b18ef5d4c48d79c
dmarquiroga@gmail.com <dmarquiroga@gmail.com>
2023-07-01 19:04:43 +0200

    Cambios en hooks para usar correctamente el user.token en la peticion post y get de useAuthHttpCall.js,,, Cambio en authApi.js de login y user porque ahora usamos la BACKEND_URL en el get y post de useAuthHttpCall.js,,, Empezando a implementar en newsApi.js votar noticias positivamente y negativamente

--- src/hooks/authApi.js
+++ src/hooks/authApi.js
@@ -1,18 +1,17 @@
 import { useUser } from '../context/UserContext';
 import useAuthHttpCall from './useAuthHttpCall';
-import { BACKEND_URL } from '../config';
 
 export function useAuthentication() {
   const { post } = useAuthHttpCall();
   const [, setUser] = useUser();
 
   const login = (email, password) =>
-    post(BACKEND_URL + '/login', { email, password }).then((response) =>
+    post('/login', { email, password }).then((response) =>
       setUser(response.data)
     );
 
   const signup = (name, surname, email, password) =>
-    post(BACKEND_URL + '/user', {
+    post('/user', {
       name,
       surname,
       email,
--- src/hooks/newsApi.js
+++ src/hooks/newsApi.js
@@ -25,4 +25,47 @@ export function useNews(selectedCategory) {
   return news;
 }
 
+// Votar una noticia positivamente
+// router.post('/news/:id/like', authUser, voteLike);
+// // Votar una noticia negativamente
+// router.post('/news/:id/dislike', authUser, voteDislike);
+
+/*
+export function useVoteLike(newsId) {
+  const [votes, setVotes] = useState([]);
+  const { post } = useAuthHttpCall();
+
+  const votePositive = async () => {
+    try {
+      const url = `/news/${newsId}/like`;
+      const data = await post(url);
+      setVotes(data.data);
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+    }
+  };
+
+  return votePositive;
+}
+
+export function useVoteDislike(newsId) {
+  const [votes, setVotes] = useState([]);
+  const { post } = useAuthHttpCall();
+
+  const voteNegative = async () => {
+    try {
+      const url = `/news/${newsId}/dislike`;
+      const data = await post(url);
+      setVotes(data.data);
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+    }
+  };
+
+  return voteNegative;
+}
+
+export default { useNews, useVoteLike, useVoteDislike };
+*/
+
 export default useNews;
